6394d6853abd0f3fc1474087ce691e2895009952
Ionică Bizău <bizauionica@gmail.com>
2015-12-28 19:40:06 +0200

    Initial

---
+++ example/index.js
@@ -0,0 +1,20 @@
+const emojiFromWord = require("../lib");
+
+console.log(emojiFromWord("house"));
+// => Match {
+//  input: 'house',
+//  score: 1,
+//  emoji:
+//   { keywords: [ 'building', 'home' ],
+//     char: '🏠',
+//     category: 'travelandplaces' },
+//  emoji_name: 'house' }
+
+console.log(emojiFromWord("door").toString());
+// => ":door:"
+
+console.log(emojiFromWord("happy").toString());
+// => ":grinning:"
+
+console.log(emojiFromWord("space").toString());
+// => ":milky_way:"
